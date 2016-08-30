0ff4c7e16b72915cd4a06d9b0060ed4d1c443098
tehsaya <toanlmse03407@fpt.edu.vn>
2016-08-31 01:59:47 +0700

    add 2 file

---
+++ OneVietnam/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OneVietnam.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult PageNotFound()
+        {
+            Response.StatusCode = 404;  //you may want to set this to 200
+            return View();
+        }
+    }
+}
